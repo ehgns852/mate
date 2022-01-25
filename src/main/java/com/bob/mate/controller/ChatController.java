7329455e3aad62f5acb28a1c30b7040a4007ae2c
ehgns852 <ehgns5668@naver.com>
2022-01-25 18:58:45 +0900

    Add 테스트 채팅 기능

---
+++ src/main/java/com/bob/mate/controller/ChatController.java
@@ -0,0 +1,16 @@
+package com.bob.mate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class ChatController {
+
+    @GetMapping("/chat")
+    public String getChat(){
+        log.info("@ChatController.GET()");
+        return "chat";
+    }
+}
