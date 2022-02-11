451fbaffc42ddfac42f8e120b957f12d36763342
sdy <vel1024@khu.ac.kr>
2022-02-11 17:52:46 +0900

    refactor: 불필요한 파일들 제거

--- src/main/java/com/bob/mate/domain/post/repository/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bob.mate.domain.post.repository;
-
-import com.bob.mate.domain.post.entity.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
